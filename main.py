e9a545724b303ad85e91489dd128debd41cc119d
Martinez-Soto-Gael <gaelmartinezsottto@gmail.com>
2025-04-28 11:20:44 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+num1=int(input("ingrese el primer valor"))
+num2=int(input("ingrese el segundo valor"))
+operacion=input("ingrese una opcion +,-,*,/")
+if operacion == '+':
+ suma = num1 + num2
+ print("Resultado:", suma)
+elif operacion == '-':
+ resta = num1 - num2
+ print("Resultado:", resta)
+elif operacion == '*':
+ multiplicacion = num1 * num2
+ print("Resultado: ", multiplicacion)
+elif operacion == '/':
+ Resultado = num1 / num2
+ print("Resultado:", Resultado)
+
+ 
