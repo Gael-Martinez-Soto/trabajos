923ea24d2b57deff1838a3ea8ce7930dc562929e
Martinez-Soto-Gael <gaelmartinezsottto@gmail.com>
2025-04-28 08:59:26 -0600

    Area del circulo.cpp

--- Area del circulo.cpp
+++ Area del circulo.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-int radio,pi,r;
+int rd,pi,r;
 cout<<"porfavor intruduzca el radio\n";
 cin>>rd;
 pi=3.1416;
-resul=rd*rd*pi;
+r=rd*rd*pi;
 cout<<"el resultado fue\n";
 cout<<r;
 
