42628ec6460ac94622da2c178c0adc593b71c037
Martinez-Soto-Gael <gaelmartinezsottto@gmail.com>
2025-04-28 08:55:09 -0600

    Area del triangulo.cpp

--- Area del triangulo.cpp.cpp
+++ Area del triangulo.cpp.cpp
@@ -1,17 +1,8 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 using namespace std;
 int main(){
     int base,altura,resul;
-    cout<<"Bienvenido a Area del triangulo\n";
     cout<<"por favor ingresar la altura\n";
     cin>> altura;
     cout<<"por favor ingrese la base";
