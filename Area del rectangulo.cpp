82d75f44969a0b02949848082553db70ce80f409
Martinez-Soto-Gael <gaelmartinezsottto@gmail.com>
2025-04-28 08:58:48 -0600

    Area del rectangulo.cpp

--- Area del rectangulo.cpp
+++ Area del rectangulo.cpp
@@ -1,24 +1,15 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int base,altura,resul;
-cout<<"bienvenido a area del rectangulo\n";
+int b,a,r;
 cout<<"porfavor intruduzca la base\n";
-cin>>base;
+cin>>b;
 cout<<"porfavor intruduzca la altura";
-cin>>altura;
-resul=base*altura;
+cin>>a;
+r=b*a;
 cout<<"el resultado fue\n";
-cout<<resul;
+cout<<r;
 
 }
